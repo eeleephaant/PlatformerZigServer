683c10d409cc0e7c4d8c18e3e403251c73d39d72
thrFrogofDream <al.kov2006@gmail.com>
2025-12-14 21:49:16 +0500

    lobby met

--- src/models/lobby.rs
+++ src/models/lobby.rs
@@ -5,4 +5,25 @@ pub struct Lobby {
     pub max_players: usize,
 }
 
-impl Lobby {}
+impl Lobby {
+    pub fn new(id: u32, max_players: usize) -> Self {
+        Self {
+            id,
+            players: Vec::new(),
+            max_players,
+        }
+    }
+    pub fn add_player(&mut self ,player: Player) -> bool {
+        if self.is_full(){
+            return false;
+        }
+        self.players.push(player);
+        return true;
+    }
+    pub fn is_full(&self) -> bool {
+        return self.players.len() >= self.max_players;
+    }
+    pub fn remove_player(&mut self, player: Player){
+        self.players.remove(player);
+    }
+}
